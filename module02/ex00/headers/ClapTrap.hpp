1f40a06e3b3b55a61289e8ca5400f91db143e326
GyasAlves <gyasminassuncao@gmail.com>
2025-11-07 18:08:58 -0300

    cpp02: ex00 - done

---
+++ module02/ex00/headers/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: galves-a <galves-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 16:43:03 by galves-a          #+#    #+#             */
+/*   Updated: 2025/11/07 17:55:04 by galves-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAP_TRAP_HPP
+#define CLAP_TRAP_HPP
+
+#include <string>
+#include <iostream>
+
+class ClapTrap {
+    
+    private:
+        std::string name;
+        int         hit;
+        int         energy;
+        int         damage;
+    
+    public:
+    
+        ClapTrap(std::string name);
+        ~ClapTrap();
+        
+        void    attack(const std::string &target);
+        void    takeDamage(unsigned int amount);
+        void    beRepaired(unsigned int amount);
+};
+
+#endif
---
+++ module02/ex00/source/ClapTrap.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: galves-a <galves-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 16:38:47 by galves-a          #+#    #+#             */
+/*   Updated: 2025/11/07 18:08:27 by galves-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name)
+{
+    this->name = name;
+    this->hit = 10;
+    this->energy = 10;
+    this->damage = 0;
+
+    std::cout << "Warrior <" << this->name << "> has been summoned!! " << std:: endl;
+}
+
+ClapTrap::~ClapTrap() {
+    std::cout << "Warrior <" << this->name << ">: has been eliminated" << std:: endl;
+}
+
+void    ClapTrap::attack(const std::string &target)
+{
+    std::cout << "Warrior <" << this->name << ">: attacks enemy " << target << std::endl;
+}
+
+void    ClapTrap::takeDamage(unsigned int amout)
+{
+    std::cout << "Warrior <" << this->name << ">: takes -" << amout << " of damage" << std::endl;
+}
+
+void    ClapTrap::beRepaired(unsigned int amout)
+{
+    std::cout << "Warrior <" << this->name << ">: receives " << amout << " point(s) of healing" << std::endl;
+}
